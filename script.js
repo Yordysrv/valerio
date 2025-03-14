855e9aa93cff2d4d8e1d04a91f4bf0255ea68b58
Yordysrv <yordis23091@gmail.com>
2025-03-14 16:18:35 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+window.onload = function () {
+    const bodyElement = document.body
+
+bodyElement.style.backgroundImage = "url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTMnZEfaEbvTE4OEvCgwcwLVNxX3AVA3tXb9g&s')"
+}
